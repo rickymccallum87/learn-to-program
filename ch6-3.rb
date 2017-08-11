066b6a5fc1c300c10b43e9d7dd9aaa1e7754048d
Ricky McCallum <rickymccallum87@gmail.com>
2017-08-11 18:33:44 +0000

    Correct leap year check and remove debug start/end values

--- ch6-3.rb
+++ ch6-3.rb
@@ -1,14 +1,14 @@
 puts 'Starting year?'
-current = 280 # gets.chomp.to_i
+current = gets.chomp.to_i
 puts 'Ending year?'
-last = 420 # gets.chomp.to_i
+final = gets.chomp.to_i
 
-while current <= last
+while current <= final
   # /4 yes, /100 no, /400 yes
   div4 = current % 4 == 0
   div100 = current % 100 == 0
   div400 = current % 400 == 0
-  if div4
+  if (div4 and not div100) or div400
     puts current
   end
 
